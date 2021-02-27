e53e4392fc7225e084b9da633c7a1c0c82449b81
“siddiq76” <siddiq.khan0786@gmail.com>
2021-02-27 09:27:05 +0530

    add

--- Linkedlist/src/main/java/com/Linked/Linkedlist/MainClass.java
+++ Linkedlist/src/main/java/com/Linked/Linkedlist/MainClass.java
@@ -6,9 +6,10 @@ public class MainClass
     {
         System.out.println( "Welcome to Linked List Operations!" );
         LinkedList linkedList = new LinkedList();
-        linkedList.addNodeAtFirst(70);
-        linkedList.addNodeAtFirst(30);
-        linkedList.addNodeAtFirst(56);
+        linkedList.addNodeAtLast(56);
+        linkedList.addNodeAtLast(70);
+//        for Adding 30 in between specify index as 1 on runtime
+        linkedList.addNodeAtIndex(30);
         linkedList.printList();
     }
 }
